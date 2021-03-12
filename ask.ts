bd2e7cbcc63803d6584b66a773334a15793e6a87
SuperManfred <Manfred.Neustifter@gmail.com>
2021-03-12 17:22:36 +1000

    ask cli

---
+++ ask.ts
@@ -0,0 +1,22 @@
+import Ask from "https://deno.land/x/ask@1.0.6/mod.ts";
+
+const ask = new Ask({
+    prefix: "",
+    suffix: "?",
+  }); 
+
+const answers = await ask.prompt([
+  {
+    name: "Name",
+    type: "input",
+    message: "Full Name",
+  },
+  {
+    name: "DOB",
+    type: "number",
+    message: "Date of Birth (ddmmyyyy)",
+  },
+]);
+
+
+console.log(answers);
