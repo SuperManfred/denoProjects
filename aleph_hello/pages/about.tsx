ca90aac7c57b43c03955671a49f66d57700f4a4b
SuperManfred <Manfred.Neustifter@gmail.com>
2021-03-15 15:00:27 +1000

    add page route /about

---
+++ aleph_hello/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from "https://esm.sh/react";
+
+export default function About() {
+  return <h1>About</h1>
+//   <p>
+//       In Aleph.js, a page is a React Component exported as default from a .js, .jsx, .ts, .tsx or .mjs file in the pages directory. Each page is associated with a route based on its file name.
+//   </p>
+//   <p>
+//       Example: this file pages/about.tsx exports a React component accessible at: host:port/about
+//   </p>;
+}
