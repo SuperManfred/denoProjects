3fe4c67b8504df94b9f573cf5668066e4aff6254
SuperManfred <Manfred.Neustifter@gmail.com>
2021-03-22 20:11:32 +1000

    :( cant make greetings work right now

--- tcpserver.ts
+++ tcpserver.ts
@@ -4,7 +4,7 @@ const hostname = "0.0.0.0";
 const port = 8080;
 const listener = Deno.listen({ hostname, port });
 console.log(
-  `Listening on ${hostname}:${port} \nin a new terminal enter this command~$ nc localhost 8080 \nthen write "echo :)"`,
+  `Listening on ${hostname}:${port} \nin a new terminal enter this command~$ nc localhost 8080 \nthen write "echo"`,
 );
 for await (const conn of listener) {
   Deno.copy(conn, conn);
